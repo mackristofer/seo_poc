202389c9aba7be25a21dc70ee10073a58dcfef56
mackristofer <mackristofer@gmail.com>
2023-03-16 16:11:26 -0300

    meta ogimagem

--- seo/src/pages/Welcome/index.tsx
+++ seo/src/pages/Welcome/index.tsx
@@ -7,7 +7,7 @@ export default function Welcome() {
                 <meta property="og:type" content="website" />
                 <meta content="Teste de preview da tag title" property="og:title" />
                 <meta property="og:description" content="Teste de preview da tag description" />
-                <meta property="og:image:secure_url" content="https://static.wixstatic.com/media/e562e7_0a84ff9b716f4fc88d1adec8de979d37~mv2.jpg/v1/fill/w_605,h_465,al_c,q_80,usm_0.66_1.00_0.01/e562e7_0a84ff9b716f4fc88d1adec8de979d37~mv2.jpg" />
+                <meta property="og:image" content="https://static.wixstatic.com/media/e562e7_0a84ff9b716f4fc88d1adec8de979d37~mv2.jpg/v1/fill/w_605,h_465,al_c,q_80,usm_0.66_1.00_0.01/e562e7_0a84ff9b716f4fc88d1adec8de979d37~mv2.jpg" />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
                 <meta property="og:locale" content="pt_BR" />
