15425daa65e3ea3aa355ec110f4d7b1db8bf2333
mackristofer <mackristofer@gmail.com>
2023-03-16 10:09:49 -0300

    secure url

--- seo/src/pages/Welcome/index.tsx
+++ seo/src/pages/Welcome/index.tsx
@@ -7,11 +7,11 @@ export default function Welcome() {
                 <meta property="og:type" content="website" />
                 <meta property="og:title" content="Teste de preview da tag title" />
                 <meta property="og:description" content="Teste de preview da tag description" />
-                <meta property="og:image" content="https://devsuperior.com.br/_next/static/images/logo-white-10059e26f600604a7b5bd7782ed7550c.svg" />
+                <meta property="og:image:secure_url" content="https://devsuperior.com.br/_next/static/images/logo-white-10059e26f600604a7b5bd7782ed7550c.svg" />
                 <meta property="og:image:width" content="1200" />
                 <meta property="og:image:height" content="630" />
                 <meta property="og:locale" content="pt_BR" />
-                <meta property="og:secure_url" content="https://seodevsuperior.netlify.app/" />
+                <meta property="og:url" content="https://seodevsuperior.netlify.app" />
                 <meta property="og:site_name" content="Teste preview site name" />
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:site" content="@devsuperior" />
