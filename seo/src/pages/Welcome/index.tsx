7997597419d2db12074f22016e42551197bc730b
mackristofer <mackristofer@gmail.com>
2023-03-16 08:52:58 -0300

    novo teste

--- seo/src/pages/Welcome/index.tsx
+++ seo/src/pages/Welcome/index.tsx
@@ -4,22 +4,17 @@ export default function Welcome() {
     return (
         <>
             <Helmet>
-                <meta charSet="UTF-8" />
-                <link rel="icon" type="image/svg+xml" href="/vite.svg" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>SEO</title>
-                <meta property='og:type' content='website' />
-                <meta property='og:title' content='Teste de preview da tag title' />
-                <meta property='og:description' content='Teste de preview da tag description' />
-                <meta property='og:image' content='https://devsuperior.com.br/_next/static/images/logo-white-10059e26f600604a7b5bd7782ed7550c.svg' />
-                <meta property='og:image:width' content='1200' />
-                <meta property='og:image:height' content='630' />
-                <meta property='og:locale' content='pt_BR' />
-                <meta property='og:url' content='https://seodevsuperior.netlify.app/' />
-                <meta property='og:site_name' content='Teste preview site name' />
-                <meta name='twitter:card' content='summary_large_image' />
-                <meta name='twitter:site' content='@devsuperior' />
-
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Teste de preview da tag title" />
+                <meta property="og:description" content="Teste de preview da tag description" />
+                <meta property="og:image" content="https://devsuperior.com.br/_next/static/images/logo-white-10059e26f600604a7b5bd7782ed7550c.svg" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:locale" content="pt_BR" />
+                <meta property="og:url" content="https://seodevsuperior.netlify.app/" />
+                <meta property="og:site_name" content="Teste preview site name" />
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@devsuperior" />
             </Helmet>
             <main className="main-body container">
                 <h2>Bem-vindos!</h2>
