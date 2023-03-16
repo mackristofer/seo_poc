1715c5029733a2cede8ecba592f32c3aba3972aa
mackristofer <mackristofer@gmail.com>
2023-03-16 01:27:34 -0300

    update site url

--- seo/src/pages/Welcome/index.tsx
+++ seo/src/pages/Welcome/index.tsx
@@ -11,7 +11,7 @@ export default function Welcome() {
                 <meta property='og:image:width' content='1200' />
                 <meta property='og:image:height' content='630' />
                 <meta property='og:locale' content='pt_BR' />
-                <meta property='og:url' content='https://devsuperior.com.br' />
+                <meta property='og:url' content='https://seodevsuperior.netlify.app/' />
                 <meta property='og:site_name' content='Teste preview site name' />
                 <meta name='twitter:card' content='summary_large_image' />
                 <meta name='twitter:site' content='@devsuperior' />
