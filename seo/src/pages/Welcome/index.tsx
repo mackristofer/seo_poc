44d4da1d31ecfa8e0f8dab513bdd4b2ad9416758
mackristofer <mackristofer@gmail.com>
2023-03-16 08:56:17 -0300

    novo teste dados og

--- seo/src/pages/Welcome/index.tsx
+++ seo/src/pages/Welcome/index.tsx
@@ -4,17 +4,12 @@ export default function Welcome() {
     return (
         <>
             <Helmet>
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="Teste de preview da tag title" />
-                <meta property="og:description" content="Teste de preview da tag description" />
-                <meta property="og:image" content="https://devsuperior.com.br/_next/static/images/logo-white-10059e26f600604a7b5bd7782ed7550c.svg" />
-                <meta property="og:image:width" content="1200" />
-                <meta property="og:image:height" content="630" />
-                <meta property="og:locale" content="pt_BR" />
-                <meta property="og:url" content="https://seodevsuperior.netlify.app/" />
-                <meta property="og:site_name" content="Teste preview site name" />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@devsuperior" />
+
+                <title>The Rock (1996)</title>
+                <meta property="og:title" content="The Rock" />
+                <meta property="og:type" content="video.movie" />
+                <meta property="og:url" content="https://www.imdb.com/title/tt0117500/" />
+                <meta property="og:image" content="https://ia.media-imdb.com/images/rock.jpg" />
             </Helmet>
             <main className="main-body container">
                 <h2>Bem-vindos!</h2>
