16b618cf3bd64cca3cde115d6f9e246ea43feb8c
mackristofer <mackristofer@gmail.com>
2023-03-16 18:03:14 -0300

    usando backend node

--- seo/src/pages/Welcome/index.tsx
+++ seo/src/pages/Welcome/index.tsx
@@ -4,9 +4,9 @@ export default function Welcome() {
     return (
         <HelmetProvider>
           <Helmet>
-                <meta property='og:type' key={'og:type'} content='website' />
-                <meta content='Teste de preview da tag title' property='og:title' key={'og:title'} />
-                <meta property='og:description' key={'og:description'} content={'Teste de preview da tag description'} />
+                <meta property='og:type' content='website' />
+                <meta content='Teste de preview da tag title' property='og:title' />
+                <meta property='og:description' content={'Teste de preview da tag description'} />
                 <meta property='og:image' content={'https://static.wixstatic.com/media/e562e7_0a84ff9b716f4fc88d1adec8de979d37~mv2.jpg/v1/fill/w_605,h_465,al_c,q_80,usm_0.66_1.00_0.01/e562e7_0a84ff9b716f4fc88d1adec8de979d37~mv2.jpg'} />
                 <meta property='og:image:width' content='1200' />
                 <meta property='og:image:height' content='630' />
